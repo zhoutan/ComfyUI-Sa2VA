a3fca98bb001c8710eb745942ab5f56e469c58d6
zhoutan <tanzhouyoyo@126.com>
2026-02-25 16:06:31 +0800

    add models dir for sa2va

--- __init__.py
+++ __init__.py
@@ -5,6 +5,15 @@ from .config import be_quiet  # Import the global config
 # Import Sa2VA node
 from .nodes.sa2va_node_tpl import Sa2VANodeTpl
 
+import folder_paths
+import os
+
+# 注册 sa2va 目录
+folder_paths.add_model_folder_path(
+    "sa2va",
+    os.path.join(folder_paths.models_dir, "sa2va")
+)
+
 # Define node class mappings
 NODE_CLASS_MAPPINGS = {
     "Sa2VANodeTpl": Sa2VANodeTpl,
